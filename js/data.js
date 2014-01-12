54efa4eafe1df1895b3bc4da6714100d41dcfa36
Ben LeVeque <ben.leveque@gmail.com>
2014-01-12 01:36:23 -0800

    more words...

--- js/data.js
+++ js/data.js
@@ -1,5 +1,355 @@
 var Data = (function() {
 
+	var starwars = {
+
+	};
+
+	var foodandbeverage = {
+		structure: [
+			'Last night, I made a ||ingredient|| ||dish|| for some friends. It was ||adj|| with a hint of ||noun||.',
+			'Have you had the ||adj|| yet ||adj|| ||dish|| at ||place||? It\'s so ||emotion||.',
+			'||place|| has the best ||adj|| ||dish||. I found it ||emotion||, however subtly ||emotion||.',
+			'Going to ||place|| was ||emotion||. I could never imagine pairing ||ingredient|| and ||ingredient|| to such ||emotion|| effect.',
+			'You know what this party needs? A ||wineadj|| ||wine||; that could really change the mood.',
+			'How do you feel about ||wine||s? I find them ||wineadj||, but also ||wineadj||.',
+			'Their ||dish|| is ||adj||, but their ||dish|| is unnecessarily ||adj||.'
+		],
+		w: [
+			'rotund',
+			'high-tannin',
+			'low-tannin',
+			'strawberry',
+			'leggy',
+			'big',
+			'black cherry'
+		],
+		adj: [
+			'oaky',
+			'rich',
+			'strong',
+			'soft',
+			'barnyardy',
+			'sweet',
+			'tangy',
+			'nutty',
+			'fruty',
+			'umami',
+			'sour',
+			'whimsical',
+			'playful',
+			'surprising',
+			'intoxicating',
+			'savory',
+			'floral',
+			'spicy',
+			'robust',
+			'juicy'
+		],
+		wine: [
+			'cab',
+			'pinot',
+			'granache',
+			'bordeaux',
+			'chardonnay',
+			'pinot gris',
+			'sauvignon blanc',
+			'pinot grigio',
+			'barollo',
+			'malbec',
+			'prosecco',
+			'champaigne',
+			'moscato',
+			'red blend',
+			'ros&#233;'
+		],
+		dish: [
+			'ragu',
+			'risotto',
+			'polenta cake',
+			'crusted ||ingredient||',
+			'fritter',
+			'fritatta',
+			'carpaccio',
+			'gratin',
+			'cake',
+			'enchilada',
+			'roll',
+			'torta',
+			'pastitsio',
+			'flatbread',
+			'crepe',
+			'paella'
+		],
+		ingredient: [
+			'butternut squash',
+			'porcini',
+			'shiitake',
+			'chantrelle',
+			'truffle',
+			'tiger prawns',
+			'Chilean sea bass',
+			'kale',
+			'ridicchio',
+			'endive',
+			'leek',
+			'potato',
+			'parmesian',
+			'bleu cheese',
+			'prosciutto',
+			'pancetta',
+			'bacon',
+			'goat cheese',
+			'duck',
+			'elk',
+			'lamb',
+			'veal',
+			'salmon',
+			'halibut',
+			'buratta',
+			'fennel',
+			'pink peppercorn',
+			'cloudberry',
+			'salmonberry',
+			'romanesco'
+		],
+		noun: [
+			'cherry',
+			'plum',
+			'oak',
+			'hickory',
+			'peach',
+			'apple',
+			'pear',
+			'rosemary',
+			'sage',
+			'thyme',
+			'taragon',
+			'blackberry'
+		],
+		emotion: [
+			'delicious',
+			'scrumptious',
+			'breathtaking',
+			'complex',
+			'intriguing',
+			'delightful',
+			'whimsical',
+			'playful',
+			'surprising',
+			'intoxicating',
+			'plebian',
+			'pedestrian'
+		],
+		place: [
+			'the French Laundry',
+			'Canlis',
+			'Bouchon',
+			'Per Se',
+			'Alder',
+			'the Chart House',
+			'Jean Georges',
+			'Smolder',
+			'Bar Sajor',
+			'Whale Winds'
+		]
+	};
+
+	foodandbeverage.wineadj = foodandbeverage.w.concat(foodandbeverage.adj);
+
+	var topical = {
+		structure: [
+			'Did you see ||person|| ||verb:2|| at ||place|| last night? It was like ||person|| ||verb:2|| all over again.',
+			'Have you heard ||person||\'s opinion on ||noun||? It\'s pretty ||adj||.',
+			'I think ||event|| has gone downhill. I remember last year when ||person|| and ||person|| ||verb:1|| onstage. It was so ||adj||.',
+			'I can\'t believe what ||person|| said about ||noun||! ||person|| and ||person|| got so ||adj|| that they ||verb:1||.',
+			'Why are ||person|| and ||person|| dating?! How on earth do they ever ||verb:0|| together?',
+			'Have you heard that ||person|| is dating ||person||? I heard about it on TMZ when ||person|| tweeted it. \#||adj||',
+			'||person|| got so ||adj|| last night. ||hashtag||'
+		],
+		verb: [
+			['twerk', 'twerked', 'twerking'],
+			['snapchat', 'snapchatted', 'snapchatting'],
+			['tweet', 'tweeted', 'tweeting'],
+			['take bath salts', 'took bath salts', 'taking bath salts'],
+			['swag out', 'swagged out', 'swagging out'],
+			['get turnt up', 'got turnt up', 'getting turnt up'],
+			['crowdsource', 'crowdsourced', 'crowdsourcing'],
+			['hobbit', 'hobbited', 'hobbiting'],
+			['LARP', 'LARPed', 'LARPing'],
+			['cosplay', 'cosplayed', 'cosplaying'],
+			['have a fight', 'had a fight', 'having a fight'],
+			['get caught on camera', 'got caught on camera', 'getting caught on camera'],
+			['sleep', 'slept', 'sleeping']
+		],
+		adj: [
+			'dope',
+			'turnt up',
+			'hipster',
+			'hood',
+			'beast',
+			'torched',
+			'twisted',
+			'pro-abortion',
+			'right-wing',
+			'left-wing',
+			'socialist'
+		],
+		person: [
+			'Miley Cyrus',
+			'Rob Ford',
+			'Justin Bieber',
+			'Dennis Rodman',
+			'Putin',
+			'Obama',
+			'Oprah',
+			'Anthony Weiner',
+			'Tiger Woods',
+			'Jennifer Lawrence',
+			'LeBron',
+			'JaVale McGee',
+			'Snoop Lion',
+			'Tom Cruise',
+			'Snoopzilla',
+			'Two Chainz',
+			'Chief Keef',
+			'Kanye West',
+			'Kim Kardashian',
+			'Kathy-Lee Gifford',
+			'One Direction',
+			'Louis C.K.',
+			'Aziz Ansari',
+			'Tina Fey',
+			'Hillary Clinton',
+			'Joss Whedon',
+			'Michael Bay',
+			'Dwayne \"The Rock\" Johnson',
+			'Kim Jong Un'
+		],
+		event: [
+			'the Grammies',
+			'the Globes',
+			'Burning Man',
+			'the Oscars',
+			'the Tonies',
+			'the ESPYs',
+			'the Teen Choice Awards',
+			'the Superbowl Halftime Show',
+			'that charity auction',
+			'the Make-a-Wish fundraiser'
+		],
+		place: [
+			'the Playboy Mansion',
+			'the Church of Scientology',
+			'Kanye\'s place',
+			'the Today Show',
+			'the O\'Reilly Factor'
+		],
+		noun: [
+			'comic-con',
+			'the government shutdown',
+			'the Tea Party',
+			'the freemasons',
+			'same-sex marriage',
+			'Google Glass',
+			'civil union',
+			'the polar vortex',
+			'the fiscal cliff',
+			'the sequester',
+			'congress',
+			'GMOs',
+			'legal marijuana',
+			'gender-neutral pronouns',
+			'fracking',
+			'selfies',
+			'Obamacare',
+			'minimum wage',
+			'the NSA',
+			'the far left',
+			'Breaking Bad',
+			'Instagram',
+			'Netflix'
+		]
+	};
+
+	var chemistry = {
+		structure: [
+			'I attempted the ||experiment|| with ||reactant|| and ||reactant||, but the whole thing ||verb:1|| in my face.',
+			'Never again will I consider mixing ||adj|| ||reactant|| and ||adj|| ||reactant|| using the ||apparatus||.',
+			'My ||apparatus|| seems to be malfunctioning. It won\'t even ||verb:0|| ||reactant|| into ||reactant||.'
+		],
+		verb: [
+			['polymerize', 'polymerized', 'polymerizing'],
+			['precipitate', 'precipitated', 'precipitating'],
+			//['turns pink', 'turned pink', 'turning pink'],
+			//['fall on the table and shatter', 'fell on the table and shattered', 'falling on the table and shattering']
+		],
+		apparatus: [
+			'bunsen burner',
+			'erlenmeyer flask'
+		],
+		reactant: [
+			'molybdenum',
+			'mild base',
+			'sulphuric acid',
+			'glacial acetic acid'
+		],
+		adj: [
+			'miscible',
+			'nonpolar',
+			'viscous',
+			'bilaterally symmetrical',
+			'trigonal planar',
+			'dextrorotary',
+			'achiral',
+			'aromatic'
+		],
+		experiment: [
+			'Diels-Alder reaction',
+			'Friedel-Crafts hydroxyalkylation'
+		],
+		noun: [
+			'cycloaddition',
+			'homolytic cleavage',
+			'backside attack'
+		]
+	};
+
+	var english = {
+		structure: [
+			'If you only read ||author|| and ||author|| more often, you would understand how ||adj|| their writing is. You could clearly see their influence in ||book||.'
+		],
+		verb: [
+			['break the fourth wall', 'broke the fourth wall', 'breaking the fourth wall'],
+			['slam', 'slammed', 'slamming'],
+			['workshop', 'workshopped', 'workshopping']
+		],
+		adj: [
+			'bathetic',
+			'bohemian',
+			'liminal',
+			'numinous',
+			'trochaic'
+		],
+		adv: [
+
+		],
+		noun: [
+			'mythos',
+			'poetaster'
+		],
+		author: [
+			'Carver',
+			'Gogol',
+			'Milton',
+			'Plath'
+		],
+		book: [
+			'Howl',
+			'Paradise Lost',
+			'Wild Animus'
+		]
+	};
+
 	var math = {
 		structure: [
 			'Given ||person||\'s method of ||verb:2|| a ||adj|| ||noun||, the result is trivial.',
@@ -69,6 +419,7 @@ var Data = (function() {
 			'Bernoulli',
 			'Cayley',
 			'Gauss',
+			'Goldwasser',
 			'Jordan',
 			'Tate',
 			'Taylor'
@@ -77,16 +428,18 @@ var Data = (function() {
 			'tubular neighborhood theorem',
 			'fundamental theorem of finitely generated abelian groups',
 			'Fermat\'s Little Theorem',
-			'the ABC conjecture'
+			'ABC conjecture'
 		]
 	}
 
 	var economics, english, geology;
 
 	return {
+		foodandbeverage: foodandbeverage,
 		economics: economics,
 		english: english,
 		geology: geology,
 		math: math,
+		topical: topical
 	};
 })();
--- js/home.js
+++ js/home.js
@@ -10,20 +10,34 @@ var Home = (function() {
 			i, r;
 		r = randInd(subject.structure.length);
 		structure = subject.structure[r];
-		return parseStructure(subject, structure);
+		var regex = /\|\|/;
+		var timesThrough = 1;
+		while(regex.test(structure)) {
+			structure = parseStructure(subject, structure,timesThrough);
+			timesThrough++;
+		}
+		structure = structure.charAt(0).toUpperCase() + structure.substring(1,structure.length);
+		return structure;
 	}
 
 	function initHandlers() {
 		$('#generateButton').on('click', function() {
-			$('#sentence').text(generateSentence(Data[$('#subject').attr('value')]));
+			pullAndGenerate();
 		});
+	}
+
+	function pullAndGenerate() {
+		var subject = $('#subject').attr('value');
+		subject = subject.replace(/ /g, '').toLowerCase();
+		$('#sentence').html(generateSentence(Data[subject]));
 	}
 
 	function load() {
 		initHandlers();
+		pullAndGenerate();
 	}
 
-	function parseStructure(subject, structure) {
+	function parseStructure(subject, structure, timesThrough) {
 		var pieces,
 			cPieces,
 			i,
@@ -31,26 +45,66 @@ var Home = (function() {
 			alreadyPicked = {};
 		pieces = structure.split("||");
 		for(i=1;i<pieces.length;i+=2) {
-			console.log(pieces[i]);
-			cPieces = pieces[i].split(':');
-			if(cPieces.length > 1) {
-				part = cPieces[0];
-				if(!alreadyPicked[part]){
-					alreadyPicked[part] = [r];
-				}
-				r = randInd(subject[part].length, alreadyPicked[part]);
-				pieces[i] = subject[part][r][parseInt(cPieces[1], 10)];
+			if(timesThrough >= 3) {
+				pieces[i] = "";
 			} else {
-				part = pieces[i];
-				if(!alreadyPicked[part]){
-					alreadyPicked[part] = [r];
+				console.log(pieces[i]);
+				cPieces = pieces[i].split(':');
+				if(cPieces.length > 1) {
+					part = cPieces[0];
+					if(!alreadyPicked[part]){
+						alreadyPicked[part] = [r];
+					}
+					r = randInd(subject[part].length, alreadyPicked[part]);
+					pieces[i] = subject[part][r][parseInt(cPieces[1], 10)];
+					alreadyPicked[part].push(r);
+				} else {
+					part = pieces[i];
+					if(!alreadyPicked[part]){
+						alreadyPicked[part] = [];
+					}
+					pieces[i] = specialBehaviors(part, subject, alreadyPicked);
 				}
-				r = randInd(subject[part].length, alreadyPicked[part]);
-				pieces[i] = subject[part][r];
 			}
-			alreadyPicked[part].push(r);
 		}
 		return pieces.join('');
+	}
+
+	function specialBehaviors(part, subject, alreadyPicked) {
+		var ret = false,
+			cat, word,
+			r, i;
+		switch(part) {
+			case 'book':
+				ret = "<i>"+part+"</i>";
+				break;
+			case 'hashtag':
+				ret = '';
+				r = randInd(5) + 1;
+				cats = [];
+				for(cat in subject) {
+					if(subject.hasOwnProperty(cat) && cat !== 'structure') {
+						cats.push(cat);
+					}
+				}
+				for(i=0;i<r;i++) {
+					r = randInd(cats.length - 1);
+					cat = subject[cats[r]];
+					console.log(cat);
+					r = randInd(cat.length - 1);
+					cat = cat[r];
+					word = (typeof cat === 'string') ? cat : cat[2];
+					// debugger;
+					ret += " #"+word.replace(/ /g,'').toLowerCase();
+				}
+				break;
+			default:
+				r = randInd(subject[part].length, alreadyPicked[part]);
+				ret = subject[part][r];
+				alreadyPicked[part].push(r);
+				break;
+		}
+		return ret;
 	}
 
 	/**
